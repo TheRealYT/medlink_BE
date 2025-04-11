60ced036f598639120ac39b3aea53958b80f848e
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-04-12 00:10:37 +0300

    feat: map routes to auth controller

---
+++ src/auth/auth.router.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import { body, pass, token } from '@/utils/parser';
+import { SendEmailDto, SignupDto, VerifyEmailDto } from '@/auth/auth.validator';
+import authController from '@/auth/auth.controller';
+
+const router = Router();
+
+router.post(
+  '/email-code',
+  body(SendEmailDto),
+  pass(authController.sendEmailCode),
+);
+
+router.post(
+  '/email-verify',
+  body(VerifyEmailDto),
+  pass(authController.verifyEmail),
+);
+
+router.post(
+  '/signup',
+  body(SignupDto),
+  token('Bearer'),
+  pass(authController.signup),
+);
+
+export default router;
