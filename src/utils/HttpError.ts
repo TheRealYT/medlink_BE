1118b03e8c55a539775908d653b30b09da584f56
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-04-11 14:36:54 +0300

    feat: add http error class for structured error response

---
+++ src/utils/HttpError.ts
@@ -0,0 +1,31 @@
+export enum ErrorCodes {
+  INVALID_INPUT = 'INVALID_INPUT',
+  NOT_FOUND = 'NOT_FOUND',
+  INTERNAL_ERROR = 'INTERNAL_ERROR',
+}
+
+export class HttpError extends Error {
+  statusCode: number;
+  errorCode?: ErrorCodes;
+  errorDetails?: object;
+
+  constructor(statusCode: number, message: string, errorCode?: ErrorCodes, errorDetails?: object) {
+    super(message);
+
+    this.statusCode = statusCode;
+    this.errorCode = errorCode;
+    this.errorDetails = errorDetails;
+  }
+}
+
+export class BadRequestError extends HttpError {
+  constructor(message: string, errorCode?: ErrorCodes, errorDetails?: object) {
+    super(400, message, errorCode, errorDetails);
+  }
+}
+
+export class NotFoundError extends HttpError {
+  constructor(message: string, errorCode?: ErrorCodes, errorDetails?: object) {
+    super(404, message, errorCode, errorDetails);
+  }
+}
