128a5ec5d99dcad38e754ff36a1558374ca4e3a3
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-04-12 10:40:38 +0300

    feat: add cache key assigner functions
    - add an access token key identifier
    - add a refresh token key identifier

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,16 @@ class AuthService {
   getSignupOtpKey(email: string, role: string) {
     return `${email}-${role}-reg-otp`;
   }
+
+  // get an access token cache key
+  getAccessTokenKey(token: string) {
+    return `access-${token}`;
+  }
+
+  // get a refresh token cache key
+  getRefreshTokenKey(token: string) {
+    return `refresh-${token}`;
+  }
 }
 
 export default new AuthService();
