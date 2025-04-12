b1439dcf09fafc676abe54a321876a95e66ab767
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-04-12 15:37:59 +0300

    refactor: change route paths

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,33 +1,17 @@
 import { Router } from 'express';
 
-import { body, pass, token } from '@/utils/parser';
-import {
-  LoginDto,
-  SendEmailDto,
-  SignupDto,
-  VerifyEmailDto,
-} from '@/auth/auth.validator';
+import { body, pass } from '@/utils/parser';
+import { LoginDto, SignupDto, VerifyEmailDto } from '@/auth/auth.validator';
 import authController from '@/auth/auth.controller';
 
 const router = Router();
 
-router.post(
-  '/email-code',
-  body(SendEmailDto),
-  pass(authController.sendEmailCode),
-);
+router.post('/signup', body(SignupDto), pass(authController.signup));
 
 router.post(
-  '/email-verify',
+  '/signup-verify',
   body(VerifyEmailDto),
   pass(authController.verifyEmail),
-);
-
-router.post(
-  '/signup',
-  body(SignupDto),
-  token('Bearer'),
-  pass(authController.signup),
 );
 
 router.post('/login', body(LoginDto), pass(authController.login));
