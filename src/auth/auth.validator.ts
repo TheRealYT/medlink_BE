abdb5fa4e6533a54206d41ab6b806ac711170686
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-04-21 10:40:29 +0300

    feat: add new validators
    - create password reset and forget password dto
    - create strong password validator for reusability

--- src/auth/auth.validator.ts
+++ src/auth/auth.validator.ts
@@ -8,6 +8,17 @@ const user_type = Yup.mixed<UserType>()
   .oneOf(Object.values(UserType) as UserType[])
   .required();
 
+const strongPassword = Yup.string()
+  .min(8, 'Password must be at least 8 characters long')
+  .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+  .matches(/\d/, 'Password must contain at least one number')
+  .matches(
+    /[!@#$%^&*(),.?":{}|<>]/,
+    'Password must contain at least one special character',
+  )
+  .required('Password is required');
+
 export const VerifyEmailDto = Yup.object().shape({
   email,
   user_type,
@@ -22,16 +33,7 @@ export const SignupDto = Yup.object().shape({
     .max(50, 'Full name cannot exceed 50 characters')
     .required('Full name is required'),
   email,
-  password: Yup.string()
-    .min(8, 'Password must be at least 8 characters long')
-    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
-    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
-    .matches(/\d/, 'Password must contain at least one number')
-    .matches(
-      /[!@#$%^&*(),.?":{}|<>]/,
-      'Password must contain at least one special character',
-    )
-    .required('Password is required'),
+  password: strongPassword,
   user_type,
 });
 
@@ -40,4 +42,16 @@ export const LoginDto = Yup.object().shape({
   user_type,
   password: Yup.string().required(),
   remember_me: Yup.boolean().default(false),
+});
+
+export const ForgetPassDto = Yup.object().shape({
+  email,
+  user_type,
+});
+
+export const ResetPassDto = Yup.object().shape({
+  email,
+  user_type,
+  password: strongPassword,
+  token: Yup.string().required(),
 });
