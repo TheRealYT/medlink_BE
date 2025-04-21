8069b4d5c963f32fa128cd64e8f078c1151fbe9f
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-04-21 16:16:00 +0300

    feat: create file uploader middleware

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,5 @@
+import path from 'node:path';
+
 export const COMPANY = 'MedLink';
 export const ADDRESS = 'Addis Ababa, Ethiopia';
+export const PROFILE_DIR = path.join(__dirname, '..', 'uploads');
