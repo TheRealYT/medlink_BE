89c123f15c5ea231025882408a8ee7e6bf096a8e
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-04-25 13:53:01 +0300

    feat: create profile image access endpoint

--- src/users/user.router.ts
+++ src/users/user.router.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
 
 import { pass } from '@/utils/parser';
 import authGuard from '@/auth/auth.guard';
@@ -7,8 +7,11 @@ import customerRouter from '@/users/customer/customer.router';
 import pharmacyRouter from '@/users/pharmacy/pharmacy.router';
 import userGuard from '@/users/user.guard';
 import { UserType } from '@/users/user.model';
+import { PROFILE_DIR } from '@/config/constants';
 
 const router = Router();
+
+router.use('/image', authGuard(), express.static(PROFILE_DIR));
 
 router.get('/profile', authGuard(), pass(userController.getProfile));
 
