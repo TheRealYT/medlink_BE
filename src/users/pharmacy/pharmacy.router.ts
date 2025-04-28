632b4c8b96bb281f056cd099419a60d59dad688a
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-04-28 09:44:48 +0300

    refactor: move route protections to the respective user type routes

--- src/users/pharmacy/pharmacy.router.ts
+++ src/users/pharmacy/pharmacy.router.ts
@@ -3,8 +3,21 @@ import { Router } from 'express';
 import { body, pass } from '@/utils/parser';
 import pharmacyController from '@/users/pharmacy/pharmacy.controller';
 import { PharmacyProfileDto } from '@/users/pharmacy/pharmacy.validator';
+import userGuard from '@/users/user.guard';
+import { UserType } from '@/users/user.model';
+import authGuard from '@/auth/auth.guard';
 
 const router = Router();
+
+// put publicly accessible routes here
+
+router.use(authGuard());
+
+// put any authenticated user accessible routes here
+
+router.use(userGuard(UserType.PHARMACIST));
+
+// put pharmacist only routes here
 
 router.get('/profile', pass(pharmacyController.getProfile));
 
--- src/users/user.router.ts
+++ src/users/user.router.ts
@@ -5,8 +5,6 @@ import authGuard from '@/auth/auth.guard';
 import userController from '@/users/user.controller';
 import customerRouter from '@/users/customer/customer.router';
 import pharmacyRouter from '@/users/pharmacy/pharmacy.router';
-import userGuard from '@/users/user.guard';
-import { UserType } from '@/users/user.model';
 import { PROFILE_DIR } from '@/config/constants';
 
 const router = Router();
@@ -15,18 +13,8 @@ router.use('/image', authGuard(), express.static(PROFILE_DIR));
 
 router.get('/profile', authGuard(), pass(userController.getProfile));
 
-router.use(
-  '/customer',
-  authGuard(),
-  userGuard(UserType.CUSTOMER),
-  customerRouter,
-);
+router.use('/customer', customerRouter);
 
-router.use(
-  '/pharmacy',
-  authGuard(),
-  userGuard(UserType.PHARMACIST),
-  pharmacyRouter,
-);
+router.use('/pharmacy', pharmacyRouter);
 
 export default router;
