512f356d2821a25e5a06ff5a5a09e639743abb56
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-05-13 16:19:24 +0300

    feat: add more health conditions

--- src/users/customer/customer.model.ts
+++ src/users/customer/customer.model.ts
@@ -6,6 +6,22 @@ export enum HealthCondition {
   Diabetes = 'diabetes',
   Hypertension = 'hypertension',
   Pregnancy = 'pregnancy',
+  Asthma = 'asthma',
+  Allergies = 'allergies',
+  Heart_Disease = 'heart_disease',
+  Epilepsy = 'epilepsy',
+  Cancer = 'cancer',
+  Chronic_Kidney_Disease = 'chronic_kidney_disease',
+  Thyroid_Disorder = 'thyroid_disorder',
+  Mental_Health_Condition = 'mental_health_condition',
+  Arthritis = 'arthritis',
+  Chronic_Pain = 'chronic_pain',
+  HIV_AIDS = 'HIV_AIDS',
+  Vision_Impairment = 'vision_impairment',
+  Hearing_Impairment = 'hearing_impairment',
+  Mobility_Issues = 'mobility_issues',
+  Lung_Disease = 'lung_disease ',
+  Liver_Disease = 'liver_disease',
 }
 
 export const OnlyHealthCondition: Partial<Record<HealthCondition, 'M' | 'F'>> =
