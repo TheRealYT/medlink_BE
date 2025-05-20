ee3cd82732a21aeb15544f1487f8d7a3af920a5e
TheRealYT <85513618+TheRealYT@users.noreply.github.com>
2025-05-20 17:34:47 +0300

    fix: reduce minimum review content length

--- src/users/pharmacy/review/review.validator.ts
+++ src/users/pharmacy/review/review.validator.ts
@@ -4,7 +4,7 @@ import { ObjectIdValidator } from '@/users/user.validator';
 
 export const ReviewDto = Yup.object({
   pharmacy_id: ObjectIdValidator.required(),
-  content: Yup.string().min(30).max(2000).optional(),
+  content: Yup.string().min(10).max(2000).optional(),
   rate: Yup.number().integer().min(1).max(5).required(),
 });
 
